f8d36036e6069a1e20d9811046d7f8a4310f0afa
Dave Hocker <AtHomeX10@gmail.com>
2019-02-18 11:08:10 -0600

    Create custom logo icon

--- resources/createicns.sh
+++ resources/createicns.sh
@@ -4,15 +4,21 @@
 # https://stackoverflow.com/questions/12306223/how-to-manually-create-icns-files-using-iconutil
 
 mkdir MyIcon.iconset
-sips -z 16 16     Icon1024.png --out MyIcon.iconset/icon_16x16.png
-sips -z 32 32     Icon1024.png --out MyIcon.iconset/icon_16x16@2x.png
-sips -z 32 32     Icon1024.png --out MyIcon.iconset/icon_32x32.png
-sips -z 64 64     Icon1024.png --out MyIcon.iconset/icon_32x32@2x.png
-sips -z 128 128   Icon1024.png --out MyIcon.iconset/icon_128x128.png
-sips -z 256 256   Icon1024.png --out MyIcon.iconset/icon_128x128@2x.png
-sips -z 256 256   Icon1024.png --out MyIcon.iconset/icon_256x256.png
-sips -z 512 512   Icon1024.png --out MyIcon.iconset/icon_256x256@2x.png
-sips -z 512 512   Icon1024.png --out MyIcon.iconset/icon_512x512.png
-cp Icon1024.png MyIcon.iconset/icon_512x512@2x.png
+sips -z 16 16     logo-512.png --out MyIcon.iconset/icon_16x16.png
+sips -z 32 32     logo-512.png --out MyIcon.iconset/icon_16x16@2x.png
+sips -z 32 32     logo-512.png --out MyIcon.iconset/icon_32x32.png
+sips -z 64 64     logo-512.png --out MyIcon.iconset/icon_32x32@2x.png
+sips -z 128 128   logo-512.png --out MyIcon.iconset/icon_128x128.png
+sips -z 256 256   logo-512.png --out MyIcon.iconset/icon_128x128@2x.png
+sips -z 256 256   logo-512.png --out MyIcon.iconset/icon_256x256.png
+sips -z 512 512   logo-512.png --out MyIcon.iconset/icon_256x256@2x.png
+sips -z 512 512   logo-512.png --out MyIcon.iconset/icon_512x512.png
+# In the original script, the base icon was 1024x1024. The online
+# icon creator couldn't produce a 1024x1024 file, so 512x512 had to be used.
+# See https://www.iconfinder.com/search/?q=icns%20file
+cp logo-512.png MyIcon.iconset/icon_512x512@2x.png
+
 iconutil -c icns MyIcon.iconset
+cp MyIcon.icns id3tag.icns
 rm -R MyIcon.iconset
+rm MyIcon.icns
